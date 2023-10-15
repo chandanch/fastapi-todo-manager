2aa11437e69645d15ca2607bc50143e1c0cedcda
chandanch <chandan.cec107@gmail.com>
2023-10-15 13:03:43 +0530

    set autoincrement for id column in rodos :bulb:

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Todo(Base):
     """
 
     __tablename__ = "todos"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
