bf2d14f007646022403fbd80f83480062ba02397
chandanch <chandan.cec107@gmail.com>
2023-10-19 23:56:53 +0530

    setup of create user api

--- router/auth.py
+++ router/auth.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from models import User
+from schemas import UserCreate
 
 router = APIRouter()
 
@@ -9,3 +11,18 @@ async def get_user():
     Auth User
     """
     return {"user": "authenticated"}
+
+
+@router.post("/users")
+async def create_user(new_user: UserCreate):
+    create_user_model = User(
+        username=new_user.username,
+        email=new_user.email,
+        first_name=new_user.first_name,
+        last_name=new_user.last_name,
+        role=new_user.role,
+        is_active=True,
+        hashed_password=new_user.password,
+    )
+
+    return create_user_model
--- schemas.py
+++ schemas.py
@@ -6,3 +6,12 @@ class TodoCreate(BaseModel):
     description: str = Field(min_length=3, max_length=100)
     priority: int = Field(gt=0, lt=6)
     is_complete: bool
+
+
+class UserCreate(BaseModel):
+    username: str = Field(min_length=3)
+    email: str
+    first_name: str = Field(min_length=2)
+    last_name: str = Field(min_length=2)
+    password: str
+    role: str = Field(min_length=3)
