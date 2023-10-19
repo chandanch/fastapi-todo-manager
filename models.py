2bcab8abf6dd1d3d93956f4586395f96ccf791b6
chandanch <chandan.cec107@gmail.com>
2023-10-19 09:03:37 +0530

    add users model with foreign key reference in todo

--- models.py
+++ models.py
@@ -1,5 +1,22 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from database import Base, db_engine
+
+
+class User(Base):
+    """
+    User Table
+    """
+
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True)
+    email = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todo(Base):
@@ -7,12 +24,14 @@ class Todo(Base):
     Class for todos
     """
 
-    __tablename__ = "todos"
+    __tablename__ = "todo"
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     is_complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
 
 # create database and tables
